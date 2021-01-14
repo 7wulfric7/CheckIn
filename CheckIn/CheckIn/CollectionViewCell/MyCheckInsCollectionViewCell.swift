502055ed1a7a66575c916123625a741b76c5487f
Deniz Adil <xdenizadil@gmail.com>
2021-01-14 03:46:10 +0100

    • UIStoryboard configured with the desired design.

---
+++ CheckIn/CheckIn/CollectionViewCell/MyCheckInsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCheckInsCollectionViewCell.swift
+//  CheckIn
+//
+//  Created by Deniz Adil on 14.1.21.
+//
+
+import UIKit
+
+class MyCheckInsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
